9a36fdd926a95926117445f020b971af21a0ef4a
Daniel Chong <danielgq.chong@gmail.com>
2024-02-25 21:54:50 +1300

    able to delete todo

--- lib/components/to_do_row.dart
+++ lib/components/to_do_row.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_drive/model/to_dos_provider.dart';
 
 import '../model/to_do.dart';
 
-class ToDoRow extends StatelessWidget {
+class ToDoRow extends StatefulWidget {
   const ToDoRow({super.key, required this.todo});
 
   final ToDo todo;
 
   @override
+  State<ToDoRow> createState() => _ToDoRowState();
+}
+
+class _ToDoRowState extends State<ToDoRow> {
+  void onPressed() {
+    Provider.of<ToDosProvider>(context, listen: false).remove(widget.todo);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Text(todo.title),
-        Text(todo.state.toString()),
-      ],
+    return ListTile(
+      title: Text(widget.todo.title),
+      subtitle: Text(widget.todo.state.toString()),
+      trailing: TextButton.icon(
+        onPressed: onPressed,
+        icon: const Icon(Icons.delete),
+        label: const Text(""),
+      ),
     );
   }
 }
